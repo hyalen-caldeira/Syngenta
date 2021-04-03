202858bc439f5602ebed55372bb191c69d14919e
Hyalen Moreira Caldeira <hyalen.moreira@gmail.com>
2021-04-03 12:32:08 -0400

    First commit ...

---
+++ src/test/java/ImageAssessmentTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ImageAssessmentTest {
+    @Test
+    public void givenAValidImage_thenWhenCountNumberOfGreenPixels_willReturnAValidNumber() {
+        ImageAssessment imageAssessment = new ImageAssessment();
+
+        assertEquals(298, imageAssessment.countGreens("Syngenta.bmp"));
+    }
+}
